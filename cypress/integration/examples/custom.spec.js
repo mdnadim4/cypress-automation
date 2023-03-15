5c3fb5facd74eee1ebbe2e1b2820826fd70d3ad6
Nadim Mahmud <mdnadim4@gmail.com>
2023-03-16 04:01:13 +0600

    test folder deleted

--- cypress/integration/examples/custom.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Custom Commands', () => {
-
-    it('Should login to application using custom commands', () => {
-        cy.visit('http://zero.webappsecurity.com/login.html')
-        cy.login('username', 'password')
-    })
-
-})
--- cypress/integration/examples/keyboard.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Keyboard press simulation", () => {
-
-    it("Should submit searchbox with pressing enter", () => {
-        cy.visit('http://zero.webappsecurity.com/index.html')
-        cy.get('#searchTerm').type('just some text {enter}')
-    })
-
-})
